dd53d40b953c4edeac079fd1b7d88e29e0db3ffa
Asfuri <eduasfuri@gmail.com>
2024-10-04 11:36:14 -0300

    Adicionado o circuito criticamente amortecido

--- RLC.c
+++ RLC.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int main() {
-  double R, L, C, sigma, omega0, omegad, s1, s2, il0, vc0, ir0, ic0, vl0, vr0, y, x, A1, A2;
+  double R, L, C, sigma, omega0, omegad, s1, s2, il0, vc0, ir0, ic0, vl0, vr0, y;
   printf("Digite seu resistor: \n");
   scanf("%lf", &R);
   printf("Digite seu indutor: \n");
@@ -18,23 +18,27 @@ int main() {
   omega0 = 1 / (sqrt(L * C));
   printf("Sigma %g\n", sigma);
   printf("Omega0 %g\n", omega0);
-
+  s1 = -sigma + sqrt(pow(sigma, 2) - pow(omega0, 2));
+  s2 = -sigma - sqrt(pow(sigma, 2) - pow(omega0, 2));
+  printf("S1 %g\n", s1);
+  printf("S2 %g\n", s2);
+  ir0 = vc0 / R;
+  ic0 = il0 - ir0;
+  y = ic0 / C;
   if (sigma > omega0) {
-    s1 = -sigma + sqrt(pow(sigma, 2) - pow(omega0, 2));
-    s2 = -sigma - sqrt(pow(sigma, 2) - pow(omega0, 2));
-    printf("S1 %g\n", s1);
-    printf("S2 %g\n", s2);
-    ir0 = vc0 / R;
-    ic0 = il0 - ir0;
-    y = ic0 / C;
+    double A1, A2;
     A2 = (y - s1 * vc0) / (s2 - s1);
     A1 = vc0 - A2;
-    printf("y %g\n", y);
     printf("A1 %g\n", A1);
     printf("A2 %g\n", A2);
     double tm = log((A1 * s1) / (-A2 * s2)) / (s2 - s1);
-    printf("%g\n", tm);
+    printf("tm %g\n", tm);
   } else if (sigma == omega0) {
-    
+    double D1, D2;
+    D2 = vc0;
+    D1 = y + (vc0 * sigma);
+    printf("D1 %g\n", D1);
+    printf("D2 %g\n", D2);
+  } else {
   }
 }
