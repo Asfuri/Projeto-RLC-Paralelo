4141ebc08067dbb8d98c4f1a1c2be824dd064df0
Asfuri <eduasfuri@gmail.com>
2024-10-04 20:48:32 -0300

    implementado tm e vtm do circuito criticamente amortecido

--- RLC.c
+++ RLC.c
@@ -2,7 +2,11 @@
 #include <stdio.h>
 
 int main() {
-  double R, L, C, sigma, omega0, omegad, s1, s2, il0, vc0, ir0, ic0, vl0, vr0, y;
+  printf("Autores: \n");
+  printf("Eduardo Asfuri \n");
+  printf("Lucas Henrique \n");
+
+  double R, L, C, sigma, omega0, omegad, s1, s2, il0, vc0, ir0, ic0, vl0, vr0, y, tm, vtm;
   printf("Digite seu resistor: \n");
   scanf("%lf", &R);
   printf("Digite seu indutor: \n");
@@ -20,25 +24,33 @@ int main() {
   printf("Omega0 %g\n", omega0);
   s1 = -sigma + sqrt(pow(sigma, 2) - pow(omega0, 2));
   s2 = -sigma - sqrt(pow(sigma, 2) - pow(omega0, 2));
-  printf("S1 %g\n", s1);
-  printf("S2 %g\n", s2);
   ir0 = vc0 / R;
   ic0 = il0 - ir0;
   y = ic0 / C;
   if (sigma > omega0) {
+    printf("S1 %g\n", s1);
+    printf("S2 %g\n", s2);
     double A1, A2;
     A2 = (y - s1 * vc0) / (s2 - s1);
     A1 = vc0 - A2;
     printf("A1 %g\n", A1);
     printf("A2 %g\n", A2);
-    double tm = log((A1 * s1) / (-A2 * s2)) / (s2 - s1);
+    tm = log(((s1 * A1) / (s2 * A2))) / (s2 - s1);
     printf("tm %g\n", tm);
+    vtm = A1 * exp(s1 * tm) + A2 * exp(s2 * tm);
+    printf("vtm %g\n", vtm);
   } else if (sigma == omega0) {
-    double D1, D2;
-    D2 = vc0;
-    D1 = y + (vc0 * sigma);
-    printf("D1 %g\n", D1);
-    printf("D2 %g\n", D2);
+    printf("S1 %g\n", s1);
+    printf("S2 %g\n", s2);
+    double A1, A2;
+    A2 = vc0;
+    A1 = y + (vc0 * sigma);
+    printf("A1 %g\n", A1);
+    printf("A2 %g\n", A2);
+    tm = (A1 - A2 * sigma) / (A1 * sigma);
+    printf("tm %g\n", tm);
+    vtm = (A1 * tm + A2) * exp(-sigma * tm);
+    printf("vtm %g\n", vtm);
   } else {
     double omegad, B1, B2;
     omegad = sqrt(pow(omega0, 2) - pow(sigma, 2));
